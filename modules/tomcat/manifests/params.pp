720641133c3b53945cec0e4537c2c953adb8ebba
Indrajeet Kumar <indrajeet@tika-btm-lp.tika-work.com>
2018-12-17 01:50:54 -0800

    dsfdsf

--- modules/tomcat/manifests/params.pp
+++ modules/tomcat/manifests/params.pp
@@ -3,7 +3,7 @@ class tomcat::params{
 $user = 'root'
 $group = 'root'
 $packages = [ 'tomcat', 'tomcat-webapps']
-$service_status = 'running '
+$service_status = 'running'
 $httpd_install = 'htop'
 $default_httpd_config = '/etx/nginx/nginx.conf'
 
